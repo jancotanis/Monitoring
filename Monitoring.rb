b556920b7ec4ae2b56688d698c35f518ca193c54
Janco Tanis <git@jancology.com>
2023-03-17 09:28:27 +0100

    don't check trial customers

--- Monitoring.rb
+++ Monitoring.rb
@@ -21,34 +21,31 @@ File.open( FileUtil.daily_file_name( "report.txt" ), "w") do |report|
 	)
 	sm = SophosMonitor.new( report, config )
 	customer_alerts = sm.run( customer_alerts )
-	report.puts "","#{sm.device_error_count} devices with issues" if sm.device_error_count > 0
 	vm = VeeamMonitor.new( report, config )
 	customer_alerts = vm.run( customer_alerts )
 	# create ticket
 	customer_alerts.each do |id, cl|
 		# we have alerts
-		if !cl.customer.is_trial?
 
-			cfg = sm.config.by_description(cl.customer.description)
-			if cfg.create_ticket
-				# remove incidents reported last run(s)
-				cfg.sophos_alerts = cl.remove_reported_incidents( cfg.sophos_alerts || [] )
-				monitoring_report = cl.report
-				if monitoring_report
-					puts "Ticket created for #{cl.name}"
-					puts monitoring_report
-					ticket = client.ticket.create(
-						title: "Monitoring: #{cl.name}",
-						state: 'new',
-						group: ENV['ZAMMAD_GROUP'],
-						priority: '2 normal',
-						customer: ENV['ZAMMAD_CUSTOMER'],
-						article: {
-							content_type: 'text/plain', # or text/html, if not given test/plain is used
-							body: monitoring_report
-						}
-					)
-				end
+		cfg = sm.config.by_description(cl.customer.description)
+		if cfg.create_ticket
+			# remove incidents reported last run(s)
+			cfg.sophos_alerts = cl.remove_reported_incidents( cfg.sophos_alerts || [] )
+			monitoring_report = cl.report
+			if monitoring_report
+				puts "Ticket created for #{cl.name}"
+				puts monitoring_report
+				ticket = client.ticket.create(
+					title: "Monitoring: #{cl.name}",
+					state: 'new',
+					group: ENV['ZAMMAD_GROUP'],
+					priority: '2 normal',
+					customer: ENV['ZAMMAD_CUSTOMER'],
+					article: {
+						content_type: 'text/plain', # or text/html, if not given test/plain is used
+						body: monitoring_report
+					}
+				)
 			end
 		end
 	end
