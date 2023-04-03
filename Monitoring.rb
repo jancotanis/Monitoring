671409eb04248a45553c0f67e7d5ab07b5228ed3
Janco Tanis <git@jancology.com>
2023-04-03 13:29:49 +0200

    new features,
    - include cloudally
    - http logging on/off with with switch
    - cleanup config usage

--- Monitoring.rb
+++ Monitoring.rb
@@ -5,7 +5,8 @@ require_relative "MonitoringConfig"
 require_relative "SophosMonitor"
 require_relative "VeeamMonitor"
 require_relative "SkykickMonitor"
-require_relative 'MonitoringModel'
+require_relative "CloudAllyMonitor"
+require_relative "MonitoringModel"
 
 Dotenv.load
 
@@ -16,7 +17,6 @@ puts "Monitor v0.9"
 
 config = MonitoringConfig.new
 options = {}
-
 o=OptionParser.new do |opts|
 	opts.banner = "Usage: Monitor.rb [options]"
 
@@ -27,9 +27,9 @@ o=OptionParser.new do |opts|
 #	opts.on("-r", "--reload", "Reload cached files") do |a|
 #		options[:reload] = a
 #	end
-#	opts.on("-l", "--log", "Log http requests") do |log|
-#		options[:log] = log
-#	end
+	opts.on("-l", "--log", "Log http requests") do |log|
+		options[:log] = log
+	end
 	opts.on_tail("-h", "-?", "--help", opts.banner) do
 		puts opts
 		exit -1
@@ -50,17 +50,19 @@ File.open( FileUtil.daily_file_name( "report.txt" ), "w") do |report|
 		url:			ENV["ZAMMAN_HOST"],
 		oauth2_token:	ENV["ZAMMAD_OAUTH_TOKEN"]
 	)
-	sm = SophosMonitor.new( report, config )
+	sm = SophosMonitor.new( report, config, options[:log] )
 	customer_alerts = sm.run( customer_alerts )
-	vm = VeeamMonitor.new( report, config )
+	vm = VeeamMonitor.new( report, config, options[:log] )
 	customer_alerts = vm.run( customer_alerts )
-	skm = SkykickMonitor.new( report, config )
+	skm = SkykickMonitor.new( report, config, options[:log] )
 	customer_alerts = skm.run( customer_alerts )
+	cam = CloudAllyMonitor.new( report, config, options[:log] )
+	customer_alerts = cam.run( customer_alerts )
 	# create ticket
 	customer_alerts.each do |id, cl|
 		# we have alerts
 
-		cfg = sm.config.by_description(cl.customer.description)
+		cfg = config.by_description(cl.customer.description)
 		if cfg.create_ticket
 			# remove incidents reported last run(s)
 			cfg.reported_alerts = cl.remove_reported_incidents( cfg.reported_alerts || [] )
@@ -83,5 +85,5 @@ File.open( FileUtil.daily_file_name( "report.txt" ), "w") do |report|
 		end
 	end
 	# update list of alerts
-	sm.config.save_config
+	config.save_config
 end
