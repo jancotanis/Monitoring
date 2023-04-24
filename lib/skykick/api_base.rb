6397f026abf1b854ace94c197a7476c9146f7f21
Janco Tanis <git@jancology.com>
2023-04-24 15:15:50 +0200

    refactor: remove unused code

--- lib/skykick/api_base.rb
+++ lib/skykick/api_base.rb
@@ -3,11 +3,4 @@ class ApiBase
 	def initialize( client )
 		@client = client
 	end
-
-	def authorize( conn )
-#		conn.request :authorization, 'Bearer', @client.connection.bearer_id
-	end
-	def authorize_request( req )
-#		req.headers['Ocp-Apim-Subscription-Key'] = @client.connection.subscription_id
-	end
 end
--- lib/veeam/api_base.rb
+++ lib/veeam/api_base.rb
@@ -3,8 +3,4 @@ class ApiBase
   def initialize( client )
     @client = client
   end
-
-  def authorize( conn )
-#	conn.request :authorization, 'Bearer', @client.connection.bearer_id
-  end
 end
