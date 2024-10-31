834b4fdf7fc2eaaa036ae27aaa6cbac9b6ef4d6f
Janco Tanis <janco.tanis@coas.com>
2024-10-31 16:01:36 +0100

    fix indents

--- MonitoringFeed.rb
+++ MonitoringFeed.rb
@@ -16,56 +16,57 @@ end
 
 class MonitoringFeed
   attr_reader :source
-	def initialize( config, feed, source )
-		@config = config
+  def initialize( config, feed, source )
+    @config = config
     @feed = feed
     @feedcache = source.downcase
     @source = source
-		# load feed and check from last item
-		@last_time = Time.new( 0 )
-
+    # load feed and check from last item
+    @last_time = Time.new( 0 )
     # backwards compatibility
     old_cache_name = "./monitor#{@feedcache}alerts.yml"
-    if File.file?( old_cache_name )
+    if File.file?(old_cache_name)
       File.rename(old_cache_name, cache_name())
     end
 
     if File.file?( cache_name() )
+      # assume last update of cache is last run of script
+      @last_time = File.mtime(cache_name())
       @alerts = YAML.load_file(cache_name()) 
     else
       @alerts = []
     end
 
-		# select companies to monitor for DTC
-		@companies = @config.entries.select{ |cfg| cfg.monitor_dtc }
-	end
+    # select companies to monitor for DTC
+    @companies = @config.entries.select{ |cfg| cfg.monitor_dtc }
+  end
 
-	def get_vulnerabilities_list( since=nil )
-		items = {}
-		since ||= @last_time
+  def get_vulnerabilities_list( since=nil )
+    items = {}
+    since ||= @last_time
 
-		URI.open( @feed ) do |rss|
-			feed = RSS::Parser.parse( rss, false )
+    URI.open( @feed ) do |rss|
+      feed = RSS::Parser.parse( rss, false )
 
-			# compress items based on link (the feed contain duplicates for some reason causing guid not unique)
-			feed.items.sort_by{ |i| i.pubDate }.each do |item|
-				items[item.link] = item unless items[item.link]
-			end
-			
-		end
-		vulnerabilities = []
-		items.values.each do |item| 
-			guid = item.link
-			if !@alerts.include? guid
-				@alerts << guid
-				if item.pubDate > since
-					vulnerabilities << Vulnerability.new( item, @companies, high_priority?( item ) ) 
-				end
-			end
-		end
-		FileUtil.write_file( cache_name(), YAML.dump( @alerts ) )
-		vulnerabilities
-	end
+      # compress items based on link (the feed contain duplicates for some reason causing guid not unique)
+      feed.items.sort_by{ |i| i.pubDate }.each do |item|
+        items[item.link] = item unless items[item.link]
+      end
+    end
+
+    vulnerabilities = []
+    items.values.each do |item| 
+      guid = item.link
+      if !@alerts.include? guid
+        @alerts << guid
+        if item.pubDate > since
+          vulnerabilities << Vulnerability.new( item, @companies, high_priority?( item ) )
+        end
+      end
+    end
+    FileUtil.write_file( cache_name(), YAML.dump( @alerts ) )
+    vulnerabilities
+  end
   
   def high_priority?( item )
     false
