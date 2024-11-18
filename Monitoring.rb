371b86a985bcc70b1317e06dc638aa8164f48742
Janco Tanis <janco.tanis@coas.com>
2024-11-18 12:16:22 +0100

    fix lint warnings

--- Monitoring.rb
+++ Monitoring.rb
@@ -1,4 +1,5 @@
-#
+# frozen_string_literal: true
+
 # 1.0	Initial version of monitoring coas saas vendor portals
 # 1.1.0	Implementation of weekly/monthly/yearly notifications for SLA actions
 # 1.2.0	Scan digital trust center
@@ -8,124 +9,129 @@
 # 1.4.1	Fix Sophos issue with missing apiHost attribute
 # 1.4.2	Fix Zabbix issue with missing zabbix_clock method
 # 1.4.3	List notifications and add NCSC feed scanning
+# 1.4.4	Code cleanup/fix lint warnings
 #
-MONITOR_VERSION = "1.4.3"
+MONITOR_VERSION = '1.4.4'
 
-require "dotenv"
-require "optparse"
+require 'dotenv'
+require 'optparse'
 require 'zammad_api'
-require_relative "utils"
-require_relative "MonitoringConfig"
-require_relative "SophosMonitor"
-require_relative "VeeamMonitor"
-require_relative "SkykickMonitor"
-require_relative "CloudAllyMonitor"
-require_relative "ZabbixMonitor"
-require_relative "Integra365Monitor"
-require_relative "MonitoringModel"
-require_relative "MonitoringSLA"
-require_relative "MonitoringDTC"
-require_relative "MonitoringNCSC"
+require_relative 'utils'
+require_relative 'MonitoringConfig'
+require_relative 'SophosMonitor'
+require_relative 'VeeamMonitor'
+require_relative 'SkykickMonitor'
+require_relative 'CloudAllyMonitor'
+require_relative 'ZabbixMonitor'
+require_relative 'Integra365Monitor'
+require_relative 'MonitoringModel'
+require_relative 'MonitoringSLA'
+require_relative 'MonitoringDTC'
+require_relative 'MonitoringNCSC'
 
 PRIO_LOW = '1 low'
 PRIO_NORMAL = '2 normal'
 PRIO_HIGH = '3 high'
 
 def file_age(name)
-  (Time.now - File.ctime(name))/(24*3600)
-end
-def garbage_collect days
-	days = 90 unless days
-	puts "- removing log files older #{days.to_i} days"
-	Dir.glob( ["*.json", "*.txt","*.yml", "*.log"] ).each do |filename|
-		if file_age(filename) > days
-			puts "  " + filename
-			File.delete( filename ) 
-		end
-	end
+  (Time.now - File.ctime(name)) / (24 * 3600)
 end
 
-def get_options config, sla
-	options = {}
-	o=OptionParser.new do |opts|
-		opts.banner = "Usage: Monitor.rb [options]"
+def garbage_collect(days = nil)
+  days ||= 90
+  puts "- removing log files older #{days.to_i} days"
+  Dir.glob(['*.json', '*.txt', '*.yml', '.log']).each do |filename|
+    if file_age(filename) > days
+      puts "  #{filename}"
+      File.delete(filename)
+    end
+  end
+end
 
-		opts.on("-s", "--sla", "Report customer SLAs") do |a|
-			config.report
-			exit 0
-		end
-		opts.on("-t", "--tenants", "Report all tenants to json") do |a|
-			options[:tenants] = a
-		end
-		opts.on("-c", "--compact", "Compact config file based on tenants") do |a|
-			puts "- compacting configuration is on"
-			options[:compact] = a
-		end
-		opts.on("-g[N]", "--garbagecollect[=N]", Float, "Remove all files older than N days, default is 90 days") do |a|
-			garbage_collect a
-		end
-	#	opts.on("-r", "--reload", "Reload cached files") do |a|
-	#		options[:reload] = a
-	#	end
-		opts.on("-n [customer,task,interval[,date]]", "--notification [customer,task,interval[,date]]", Array, "Add customer notification of list them if no arguments given") do |a|
-      if a
-        options[:customer]     = a[0].to_s.strip
-        options[:task]         = a[1].to_s.strip
-        options[:interval]     = a[2].to_s.strip
-        options[:date]         = a[3]
-        options[:notification] = a
+def get_options(config, sla)
+  options = {}
+  o = OptionParser.new do |opts|
+    opts.banner = 'Usage: Monitor.rb [options]'
+
+    opts.on('-s', '--sla', 'Report customer SLAs') do |_arg|
+      config.report
+      exit 0
+    end
+    opts.on('-t', '--tenants', 'Report all tenants to json') do |arg|
+      options[:tenants] = arg
+    end
+    opts.on('-c', '--compact', 'Compact config file based on tenants') do |arg|
+      puts '- compacting configuration is on'
+      options[:compact] = arg
+    end
+    opts.on('-g[N]', '--garbagecollect[=N]', Float, 'Remove all files older than N days, default is 90 days') do |arg|
+      garbage_collect(arg)
+    end
+    #	opts.on("-r", "--reload", "Reload cached files") do |arg|
+    #		options[:reload] = arg
+    #	end
+    opts.on(
+      '-n [customer,task,interval[,date]]', '--notification [customer,task,interval[,date]]', Array,
+      'Add customer notification of list them if no arguments given'
+    ) do |arg|
+      if arg
+        options[:customer]     = arg[0].to_s.strip
+        options[:task]         = arg[1].to_s.strip
+        options[:interval]     = arg[2].to_s.strip
+        options[:date]         = arg[3]
+        options[:notification] = arg
         sla.add_interval_notification options[:customer], options[:task], options[:interval], options[:date]
       else
         sla.report
       end
-			exit 0
-		end
-		opts.on("-l", "--log", "Log http requests") do |log|
-			puts "- API logging turned on"
-			options[:log] = log
-		end
-		opts.on_tail("-h", "-?", "--help", opts.banner) do
-			puts opts
-			exit 0
-		end
-	end
-	o.parse!
-	options
+      exit 0
+    end
+    opts.on('-l', '--log', 'Log http requests') do |log|
+      puts '- API logging turned on'
+      options[:log] = log
+    end
+    opts.on_tail('-h', '-?', '--help', opts.banner) do
+      puts opts
+      exit 0
+    end
+  end
+  o.parse!
+  options
 rescue OptionParser::ParseError => e
   puts "ERROR: #{e}\n\n"
   puts o
   exit -1
 end
 
-def monitors_do report, config, options, &block
-	if !@monitors
-		@monitors = []
-		
-		[SophosMonitor, VeeamMonitor, SkykickMonitor, CloudAllyMonitor, ZabbixMonitor, Integra365Monitor].each do |klass|
-			@monitors << klass.new( report, config, options[:log] )
-		rescue Faraday::Error => e
-			puts "** Error instantiating #{klass.name}"
-			puts e
-			puts e.response[:body] if e.response
-		end
-	end
-	@monitors.each do |m|
-		block.call m
-	end
+def monitors_do(report, config, options, &block)
+  unless @monitors
+    @monitors = []
+
+    [SophosMonitor, VeeamMonitor, SkykickMonitor, CloudAllyMonitor, ZabbixMonitor, Integra365Monitor].each do |klass|
+      @monitors << klass.new(report, config, options[:log])
+    rescue Faraday::Error => e
+      puts "** Error instantiating #{klass.name}"
+      puts e
+      puts e.response[:body] if e.response
+    end
+  end
+  @monitors.each do |m|
+    block.call m
+  end
 end
 
 def report_tenants(report, config, options)
-	puts "- report tenants"
-	monitors_do(report, config, options) do |m|
-		m.report_tenants
-	end
+  puts '- report tenants'
+  monitors_do(report, config, options) do |m|
+    m.report_tenants
+  end
 end
 
-def run_monitors( report, config, options )
-	customer_alerts  = {}
+def run_monitors(report, config, options)
+	customer_alerts = {}
 
 	monitors_do(report, config, options) do |m|
-		customer_alerts = m.run( customer_alerts )
+		customer_alerts = m.run(customer_alerts)
 	rescue Faraday::Error => e
 		puts "** Error running #{m.class.name}"
 		puts e
@@ -134,82 +140,82 @@ def run_monitors( report, config, options )
 	customer_alerts
 end
 
-def create_ticket zammad_client, title, text, ticket_prio=PRIO_NORMAL, ticket_tag=nil
-	ticket = nil
-	if !"DEBUG".eql? ENV["MONITORING"]
-		ticket = zammad_client.ticket.create(
-			title: title,
-			state: 'new',
-			group: ENV['ZAMMAD_GROUP'],
-			priority: ticket_prio,
-			customer: ENV['ZAMMAD_CUSTOMER'],
-			article: {
-				content_type: 'text/plain', # or text/html, if not given test/plain is used
-				body: text
-			}, 
-			tags: ticket_tag
-		)
-	end
-	puts "Ticket created #{title}/#{ticket_prio}"
-	puts text
-	ticket
+def create_ticket(zammad_client, title, text, ticket_prio = PRIO_NORMAL, ticket_tag = nil)
+  ticket = nil
+  unless 'DEBUG'.eql? ENV['MONITORING']
+    ticket = zammad_client.ticket.create(
+      title: title,
+      state: 'new',
+      group: ENV['ZAMMAD_GROUP'],
+      priority: ticket_prio,
+      customer: ENV['ZAMMAD_CUSTOMER'],
+      article: {
+        content_type: 'text/plain', # or text/html, if not given test/plain is used
+        body: text
+      },
+      tags: ticket_tag
+    )
+  end
+  puts "Ticket created #{title}/#{ticket_prio}"
+  puts text
+  ticket
 end
 
-puts "Monitor v#{MONITOR_VERSION} - #{Time.now}",""
+puts "Monitor v#{MONITOR_VERSION} - #{Time.now}", ''
 
 # use environment from .env if any
 Dotenv.load
 config = MonitoringConfig.new
-sla = MonitoringSLA.new( config )
+sla = MonitoringSLA.new(config)
 options = get_options(config,sla)
-feeds = [MonitoringDTC.new( config ), MonitoringNCSC.new( config )]
+feeds = [MonitoringDTC.new(config), MonitoringNCSC.new(config)]
 
-File.open( FileUtil.daily_file_name( "report.txt" ), "w") do |report|
-	client = ZammadAPI::Client.new(
-		url:          ENV['ZAMMAD_HOST'] || ENV['ZAMMAN_HOST'],
-		oauth2_token: ENV["ZAMMAD_OAUTH_TOKEN"]
-	)
-	report_tenants( report, config, options ) if options[:tenants]
+File.open(FileUtil.daily_file_name('report.txt'), 'w') do |report|
+  client = ZammadAPI::Client.new(
+    url:          ENV['ZAMMAD_HOST'] || ENV['ZAMMAN_HOST'],
+    oauth2_token: ENV['ZAMMAD_OAUTH_TOKEN']
+  )
+  report_tenants(report, config, options) if options[:tenants]
 
-	customer_alerts  = run_monitors( report, config, options )
-	# create ticket
-	last = ""
-	sorted = customer_alerts.values.sort_by{ |cl| cl.customer.description.upcase }
-	sorted.each do |cl|
-		# we have alerts
+  customer_alerts = run_monitors(report, config, options)
+  # create ticket
+  last = ''
+  sorted = customer_alerts.values.sort_by { |cl| cl.customer.description.upcase }
+  sorted.each do |cl|
+    # we have alerts
 
-		cfg = config.by_description(cl.customer.description)
-		if cfg.create_ticket
-			# remove incidents reported last run(s)
-			puts cfg.description unless last.eql? cfg.description
-			last = cfg.description
-			cfg.reported_alerts = cl.remove_reported_incidents( cfg.reported_alerts || [] )
-			monitoring_report = cl.report
-			if monitoring_report
-				ticket = create_ticket client, "Monitoring: #{cl.name}", monitoring_report, PRIO_NORMAL, cl.source
-			end
-		end
-	end
+    cfg = config.by_description(cl.customer.description)
+    if cfg.create_ticket
+      # remove incidents reported last run(s)
+      puts cfg.description unless last.eql? cfg.description
+      last = cfg.description
+      cfg.reported_alerts = cl.remove_reported_incidents(cfg.reported_alerts || [])
+      monitoring_report = cl.report
+      if monitoring_report
+        _ticket = create_ticket client, "Monitoring: #{cl.name}", monitoring_report, PRIO_NORMAL, cl.source
+      end
+    end
+  end
 
-	a = sla.get_periodic_alerts
-	a.each do |notification|
-		if notification.config.create_ticket
-			ticket = create_ticket client, "Monitoring: #{notification.config.description}", notification.description, PRIO_NORMAL, 'NOTIFICATION'
-		end
-	end
+  a = sla.load_periodic_alerts
+  a.each do |notification|
+    if notification.config.create_ticket
+      ticket = create_ticket client, "Monitoring: #{notification.config.description}", notification.description, PRIO_NORMAL, 'NOTIFICATION'
+    end
+  end
   feeds.each do |feed|
     a = feed.get_vulnerabilities_list
     a.each do |vulnerability|
-      if vulnerability.high_priority?
-        prio = PRIO_HIGH 
+      prio = if vulnerability.high_priority?
+        PRIO_HIGH
       else
-        prio = PRIO_NORMAL
+        PRIO_NORMAL
       end
-      ticket = create_ticket client, "Monitoring: #{vulnerability.title}", vulnerability.description, prio, feed.source
+      ticket = create_ticket(client, "Monitoring: #{vulnerability.title}", vulnerability.description, prio, feed.source)
     end
   end
-	
-	# update list of alerts
-	config.compact! if options[:compact]
-	config.save_config
+
+  # update list of alerts
+  config.compact! if options[:compact]
+  config.save_config
 end
--- MonitoringTest.rb
+++ MonitoringTest.rb
@@ -1,97 +1,84 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require 'minitest/spec'
-require "Date"
-require_relative "MonitoringConfig"
-require_relative "MonitoringSLA"
+require 'Date'
+require_relative 'MonitoringConfig'
+require_relative 'MonitoringSLA'
 
-TenantMock = Struct.new(:id, :description, :endpoints, :is_trial?)
+TenantMock = Struct.new(:id, :description, :endpoints, :trial?)
 
 describe '#1 config' do
-	before do
-		@cfg = MonitoringConfig.new
-	end
+  before do
+    @cfg = MonitoringConfig.new
+  end
 
-	it "#1.1 load" do
-		begin
-			@cfg.load_config( "Test", [] )
-			company = 'VDH Company B.V.'
-			assert value(@cfg.by_description( company ).description).must_equal company, "1.1.1 load []; not adding"
-			company = 'Test Company'
-			assert_nil @cfg.by_description( company ), "1.2 load []; no test company"
-			@cfg = MonitoringConfig.new
-			@cfg.load_config( "Test", [TenantMock.new("0", company, [], false)] )
-			assert value(@cfg.by_description( company ).description).must_equal company, "1.1.3 load []; adding test comp"
-			@cfg.delete_entry @cfg.by_description( company )
-			assert_nil @cfg.by_description( company ), "1.4 cfg delete entry"
-			@cfg.save_config
-		end
-	end
-	it "#1.2 find by name" do
-		begin
-			company = 'VDH Company B.V.'
-			@cfg = MonitoringConfig.new
-			@cfg.load_config( "Test", [] )
-			assert value(@cfg.by_description( company ).description).must_equal company, "1.2.1 check find by company"
-		end
-	end
+  it '#1.1 load' do
+    begin
+      @cfg.load_config('Test', [])
+      company = 'VDH Company B.V.'
+      assert value(@cfg.by_description(company).description).must_equal company, '1.1.1 load []; not adding'
+      company = 'Test Company'
+      assert_nil @cfg.by_description(company), '1.2 load []; no test company'
+      @cfg = MonitoringConfig.new
+      @cfg.load_config("Test", [TenantMock.new('0', company, [], false)])
+      assert value(@cfg.by_description(company).description).must_equal company, '1.1.3 load []; adding test comp'
+      @cfg.delete_entry @cfg.by_description(company)
+      assert_nil @cfg.by_description(company), '1.4 cfg delete entry'
+      @cfg.save_config
+    end
+  end
+  it '#1.2 find by name' do
+    begin
+      company = 'VDH Company B.V.'
+      @cfg = MonitoringConfig.new
+      @cfg.load_config('Test', [])
+      assert value(@cfg.by_description(company).description).must_equal company, '1.2.1 check find by company'
+    end
+  end
 end
 
 describe '#2 utils' do
-	it '#2.1.1 FileUtils' do
-		begin
-			assert value(FileUtil.daily_file_name( "error.log" )).must_equal 'error-'+FileUtil.timestamp+'.log', "2.1.1 daily name"
-		end
-	end
-	it '#2.1.2 FileUtils' do
-		begin
-			assert value(FileUtil.daily_module_name( "" )).must_equal 'string-'+FileUtil.timestamp+'.log', "2.1.2 daily module name"
-		end
-	end
-	it '#2.2 Struct' do
-		begin
-			TestStruct = Struct.new( :raw_data )
-			string = '{"id":"id-0", "desc":{"someKey":"someValue","anotherKey":"value"},"main_item":{"stats":{"a":8,"b":12,"c":10}}}'
-			t = TestStruct.new( JSON.parse( string ) )
-			assert value( t.property( "key" ) ).must_equal "", "2.2.1 property not exist"
-			assert value( t.property( "id" ) ).must_equal 'id-0', "2.2.1 property "
-			assert value( t.property( "desc.someKey" ) ).must_equal 'someValue', "2.2.1 nested property"
-			assert value( t.property( "main_item.stats.a" ) ).must_equal "8", "2.2.1 nested property"
-		end
-	end
+  it '#2.1.1 FileUtils' do
+    assert value(FileUtil.daily_file_name('error.log')).must_equal 'error-'+FileUtil.timestamp+'.log', "2.1.1 daily name"
+  end
+  it '#2.1.2 FileUtils' do
+    assert value(FileUtil.daily_module_name("")).must_equal 'string-'+FileUtil.timestamp+'.log', "2.1.2 daily module name"
+  end
+  it '#2.2 Struct' do
+    TestStruct = Struct.new(:raw_data)
+    string = '{"id":"id-0", "desc":{"someKey":"someValue","anotherKey":"value"},"main_item":{"stats":{"a":8,"b":12,"c":10}}}'
+    t = TestStruct.new(JSON.parse(string))
+    assert value(t.property("key")).must_equal "", "2.2.1 property not exist"
+    assert value(t.property("id")).must_equal 'id-0', "2.2.1 property "
+    assert value(t.property("desc.someKey")).must_equal 'someValue', "2.2.1 nested property"
+    assert value(t.property("main_item.stats.a")).must_equal "8", "2.2.1 nested property"
+  end
 end
 
 describe '#3 SLA periods' do
-	it '#3.1 weekly' do
-		begin
-			assert !WEEKLY.is_due?( Date.today )
-			assert !WEEKLY.is_due?( Date.today-1 )
-			assert WEEKLY.is_due?( Date.today-7 )
-			assert WEEKLY.is_due?( Date.today-100 )
-		end
-	end
-	it '#3.1 monthly' do
-		begin
-			assert !MONTHLY.is_due?( Date.today )
-			assert !MONTHLY.is_due?( Date.today - 1 )
-			assert MONTHLY.is_due?( Date.today - 30 )
-			assert MONTHLY.is_due?( Date.today - 100 )
-		end
-	end
-	it '#3.1 quarterly' do
-		begin
-			assert !QUARTERLY.is_due?( Date.today )
-			assert !QUARTERLY.is_due?( Date.today - 90 )
-			assert QUARTERLY.is_due?( Date.today - 91 )
-			assert QUARTERLY.is_due?( Date.today - 100 )
-		end
-	end
-	it '#3.1 yearly' do
-		begin
-			assert !YEARLY.is_due?( Date.today )
-			assert !YEARLY.is_due?( Date.today - 90 )
-			assert YEARLY.is_due?( Date.today - 365 )
-			assert YEARLY.is_due?( Date.today - 999 )
-		end
-	end
-
+  it '#3.1 weekly' do
+    assert !WEEKLY.due?(Date.today)
+    assert !WEEKLY.due?(Date.today-1)
+    assert WEEKLY.due?(Date.today-7)
+    assert WEEKLY.due?(Date.today-100)
+  end
+  it '#3.1 monthly' do
+    assert !MONTHLY.due?(Date.today)
+    assert !MONTHLY.due?(Date.today - 1)
+    assert MONTHLY.due?(Date.today - 30)
+    assert MONTHLY.due?(Date.today - 100)
+  end
+  it '#3.1 quarterly' do
+    assert !QUARTERLY.due?(Date.today)
+    assert !QUARTERLY.due?(Date.today - 90)
+    assert QUARTERLY.due?(Date.today - 91)
+    assert QUARTERLY.due?(Date.today - 100)
+  end
+  it '#3.1 yearly' do
+    assert !YEARLY.due?(Date.today)
+    assert !YEARLY.due?(Date.today - 90)
+    assert YEARLY.due?(Date.today - 365)
+    assert YEARLY.due?(Date.today - 999)
+  end
 end
